d23c3919ca2f54575636511262b22caa24423ee9
SampabloC <sampablo-jisc@outlook.com>
2021-07-12 20:16:10 -0500

    Add routes
    I add some routes to link every view and use resources to administrators and employees.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'employees#index'
+  get '/administrators', to: 'administrators#index'
+  get '/administrators/manageEmployees', to: 'administrators#manageEmployees'
+  get '/administrators/manageCompany', to: 'administrators#manageCompany'
+  get '/administrators/reports', to: 'administrators#reports'
+  resources :employees, :administrators
 end
