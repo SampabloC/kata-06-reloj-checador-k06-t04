61456c81d14ce46abf29c6b9d5bb28f18b73c5a6
SampabloC <sampablo-jisc@outlook.com>
2021-07-13 19:01:45 -0500

    Add employee function implementedAdd employee function implemented
    The roles that correspond to the employee controller have been created.
    Within the create function, what is necessary was implemented to insert a new employee into the database.
    The view was also created with the form that corresponds to a new user
    
    These changes were implemented to be able to insert a new employee into the database
    
    Co-authored-by: RobertoCarMed <betocarrasco3652@gmail.com>
    Co-authored-by: mrivh <m.alicia.riv@gmail.com>

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -5,4 +5,33 @@ class EmployeesController < ApplicationController
   def index
     @employees = Employee.all
   end
+
+  def new
+    @employees = Employee.new
+  end
+
+  def show
+
+  end
+
+  def create
+    data = params.require(:employee)
+    employee = Employee.new
+    employee.nameEmployee = data["nameEmployee"]
+    employee.emailEmployee = data["emailEmployee"]
+    employee.positionEmployee = data["positionEmployee"]
+    employee.attendanceDate = data["attendanceDate"]
+    employee.administrator_id = 1
+
+    #employee.administrator_id = data["1"]
+    #Employee.create(data)
+    #.permit(:employeeNumber, :nameEmployee, :emailEmployee, :positionEmployee, :attendanceDate, :administrator_id)
+    employee.save
+
+    redirect_to root_path
+  end
+
+  def edit
+
+  end
 end
