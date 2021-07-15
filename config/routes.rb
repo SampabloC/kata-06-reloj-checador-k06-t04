09e4569c31aaab8f5aeb501c30c66e8fced95d93
SampabloC <sampablo-jisc@outlook.com>
2021-07-15 17:08:03 -0500

    Add controller, model and view for Company
    We add Company files using 'rails generate controller'.
    Modify routes.rb to use Company resources.
    Add a button on company/index to go to administrator view.
    
    Co-authored-by: RobertoCarMed <betocarrasco3652@gmail.com>
    Co-authored-by: mrivh <m.alicia.riv@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
-  get '/administrators/manageEmployees', to: 'administrators#manageEmployees'
   get '/administrators/manageCompany', to: 'administrators#manageCompany'
   get '/administrators/reports', to: 'administrators#reports'
-  resources :employees, :administrators
+  get '/administrators/validation', to: 'administrators#validation'
+  root "administrators#check"
+  resources :employees, :administrators, :companies
 end
