b3b40eb679e19404f6cb3ca95832fe0244b3d58e
SampabloC <sampablo-jisc@outlook.com>
2021-07-15 18:08:56 -0500

    Add CRUD functions to Company branch
    Add in company controller all functions for CRUD.
    Add form view to create and edit company data.
    Add Confirmation message before delete.
    
    Co-authored-by: RobertoCarMed <betocarrasco3652@gmail.com>
    Co-authored-by: mrivh <m.alicia.riv@gmail.com>

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,3 +1,43 @@
 class CompaniesController < ApplicationController
-  def index; end
+  def index
+    @company = Company.all
+  end
+
+  def new
+    @company = Company.new
+  end
+
+  def create
+    company = Company.new(company_params)
+    company.administrator_id = "1"
+    company.save
+    redirect_to companies_path
+  end
+
+  def edit
+    set_companies
+  end
+
+  def update
+    set_companies
+    @company.update(company_params)
+    redirect_to companies_path
+  end
+
+  def destroy
+    set_companies
+    @company.destroy
+
+    redirect_to companies_path
+  end
+  
+  private
+
+  def company_params
+    params.require(:company).permit(:name_company, :address)
+  end
+
+  def set_companies
+    @company = Company.find(params[:id])
+  end
 end
