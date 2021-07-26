7d92e76c679dd0ae9cdc3b51340e95c011de7f47
RobertoCarMed <betocarrasco3652@gmail.com>
2021-07-26 17:40:25 -0500

    Check in function created

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,5 +1,24 @@
 class ReportsController < ApplicationController
-  before_action :validate_url
+  before_action :validate_url, only: [:index]
   
   def index; end
+
+  def create
+    @employees = Employee.where(status: true) # Empleados activos
+
+    @employees.each do |employee| # Iteración entre todos los empleados actuvos
+      number_employee = params['employee_number'].to_i
+
+      if employee.employee_number == number_employee
+        report = Report.new
+
+        report.check_in = Time.now
+        report.employee_id = employee.id
+
+        report.save
+      end
+    end
+
+    redirect_to root_path
+  end
 end
